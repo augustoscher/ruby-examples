e139a2624efccc06399900b645a8a7adde5451b2
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2020-02-04 12:25:51 -0300

    test

--- conditions-structure.rb
+++ conditions-structure.rb
@@ -9,6 +9,12 @@ else
   puts 'a == b' 
 end
 
+if not a == b
+  puts 'a <> b' 
+else
+  puts 'a == b' 
+end
+
 puts '==========' 
 puts '' 
 
@@ -24,4 +30,17 @@ when 3 .. 12
     puts 'Mar to Dec'
 else 
     puts 'Invalid Month'
+end
+
+case month 
+when 1..3
+  puts 'Você nasceu no começo do ano'
+when 9..12
+  puts 'Você nasceu no final do ano'
+when 4..6
+  puts 'Você nasceu na primeira metade do ano'
+when 7..9
+  puts 'Você nasceu na segunda metade do ano!'
+else 
+  puts 'Não foi possível identificar'
 end
