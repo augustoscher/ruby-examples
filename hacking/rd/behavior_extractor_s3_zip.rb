de3d54f2f784a22f016b6254ae6897164f9186bc
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-05-19 14:58:27 -0300

    campaign behavior extractor

--- hacking/rd/behavior_extractor_s3_zip.rb
+++ hacking/rd/behavior_extractor_s3_zip.rb
@@ -1,25 +1,41 @@
-zipper          = Zip::CsvZipStream.new
-zipper.filename = 'rdstation-campaign-behavior-extractor.csv'
-zipper.prepare
-zipper.start
+#EXECUTION EXAMPLE
+## e = CampaignInfoExtractorS3Bucket.new 2
+## e.execute
 
+class CampaignInfoExtractorS3Bucket
 
-campaigns = Campaign.where("created_at > CURRENT_DATE - INTERVAL '30 days'").select(:id, :name, :created_at, :selected_segmentations, :except_segmentations)
+  def initialize(last_considered_days)
+    @last_considered_days = last_considered_days
+  end
 
-zipper << %w(id, created_at, selected_segmentations, except_segmentations)
+  def execute
+    export Campaign.where("created_at > CURRENT_DATE - INTERVAL '#{@last_considered_days} days'")
+                   .select(:id, :name, :created_at, :selected_segmentations, :except_segmentations)
+  end
 
-campaigns.each do |campaign|
-  zipper << [
-    campaign.id,
-    campaign.name,
-    campaign.selected_segmentations.size,
-    campaign.except_segmentations.size
-  ]
+  private
+
+  def export(campaigns)
+    zipper = Zip::CsvZipStream.new
+    zipper.filename = 'rdstation-campaign-behavior-extractor.csv'
+    zipper.prepare
+    zipper.start
+
+    zipper << %w(id, created_at, selected_segmentations, except_segmentations)
+
+    campaigns.each do |campaign|
+      zipper << [
+        campaign.id,
+        campaign.name,
+        campaign.selected_segmentations.size,
+        campaign.except_segmentations.size
+      ]
+    end
+
+    file = zipper.wait_until_finished!
+    aws  = Aws::S3::Resource.new(region: 'us-east-1').bucket('rdstation-sandbox')
+    obj  = aws.object('rdstation-campaign-behavior-extractor.zip')
+    obj.upload_file(file, acl: 'public-read')
+    obj.public_url
+  end
 end
-
-file = zipper.wait_until_finished!
-aws  = Aws::S3::Resource.new(region: 'us-east-1').bucket('rdstation-sandbox')
-obj  = aws.object('rdstation-campaign-behavior-extractor.zip')
-
-obj.upload_file(file, acl: 'public-read')
-obj.public_url
